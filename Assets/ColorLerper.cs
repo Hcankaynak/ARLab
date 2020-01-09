28b26e3722f4e068f5b772b3441ddbeefd6e4d91
Hüseyincan Kaynak <h.cankaynak@gmail.com>
2020-01-09 14:20:29 +0300

    color lerp added.

---
+++ Assets/ColorLerper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerper : MonoBehaviour
+{
+
+    public float speed = 0.01f;
+    public Color newColor;
+    public bool active = false;
+    public bool activation = false;
+    float startTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activation)
+        {
+            active = true;
+            activation = false;
+            startTime = Time.time;
+        }
+        if (active)
+        {
+            float t = (Time.time - startTime) * speed;
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(gameObject.GetComponent<MeshRenderer>().material.color, newColor, t);
+            if (gameObject.GetComponent<MeshRenderer>().material.color == newColor)
+            {
+                active = false;
+            }
+        }
+    }
+
+}
