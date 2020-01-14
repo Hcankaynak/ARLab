01c9c942a8e732bc2e6eb2e1fee6f409a5b16800
Hüseyincan Kaynak <h.cankaynak@gmail.com>
2020-01-14 19:49:17 +0300

    questions have been added. for first exp

--- Assets/QuestionController.cs
+++ Assets/QuestionController.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.Events;
 
 public class QuestionController : MonoBehaviour
 {
-    
+    private UnityAction FalseAction;
+    private UnityAction TrueAction;
+    public GameObject True;
+    public GameObject False;
 	public TextMeshProUGUI first;
+    public GameObject first1;
 	public TextMeshProUGUI second;
-	public TextMeshProUGUI third;
-	public TextMeshProUGUI fourth;
+    public GameObject second2;
+    public TextMeshProUGUI third;
+    public GameObject third3;
+    public TextMeshProUGUI fourth;
+    public GameObject forth4;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        TrueAction = openTruePanel;
+        FalseAction = openFalsePanel;
     }
 
     // Update is called once per frame
@@ -24,32 +32,49 @@ public class QuestionController : MonoBehaviour
     {
 		if (PlayerPrefs.GetInt("CurrentLevel") == 1)
 		{
-            first.SetText("2");
-            second.SetText("5");
-            third.SetText("9");
-            fourth.SetText("14");
-			
-		}
-		else if (PlayerPrefs.GetInt("CurrentLevel") == 2)
-		{
-            first.SetText("3");
-            second.SetText("5");
-            third.SetText("7");
-            fourth.SetText("11");
-        }
-        else if (PlayerPrefs.GetInt("CurrentLevel") == 3)
-        {
             first.SetText("1");
             second.SetText("5");
             third.SetText("9");
-            fourth.SetText("11");
+            fourth.SetText("14");
+            first1.GetComponent<Button>().onClick.AddListener(TrueAction);
+            second2.GetComponent<Button>().onClick.AddListener(FalseAction);
+            third3.GetComponent<Button>().onClick.AddListener(FalseAction);
+            forth4.GetComponent<Button>().onClick.AddListener(FalseAction);
+            //first1.onClick.AddListener(myAction);
+
+        }
+		else if (PlayerPrefs.GetInt("CurrentLevel") == 2)
+		{
+            first.SetText("0,4");
+            second.SetText("2,6");
+            third.SetText("5,3");
+            fourth.SetText("11,9");
+            first1.GetComponent<Button>().onClick.AddListener(FalseAction);
+            second2.GetComponent<Button>().onClick.AddListener(TrueAction);
+            third3.GetComponent<Button>().onClick.AddListener(FalseAction);
+            forth4.GetComponent<Button>().onClick.AddListener(FalseAction);
+        }
+        else if (PlayerPrefs.GetInt("CurrentLevel") == 3)
+        {
+            first.SetText("2.02");
+            second.SetText("5,45");
+            third.SetText("9,90");
+            fourth.SetText("11.0");
+            first1.GetComponent<Button>().onClick.AddListener(FalseAction);
+            second2.GetComponent<Button>().onClick.AddListener(TrueAction);
+            third3.GetComponent<Button>().onClick.AddListener(FalseAction);
+            forth4.GetComponent<Button>().onClick.AddListener(FalseAction);
         }
         else if (PlayerPrefs.GetInt("CurrentLevel") == 4)
         {
-            first.SetText("2");
-            second.SetText("4");
-            third.SetText("8");
-            fourth.SetText("14");
+            first.SetText("2,5");
+            second.SetText("4,1");
+            third.SetText("8,3");
+            fourth.SetText("14,6");
+            first1.GetComponent<Button>().onClick.AddListener(FalseAction);
+            second2.GetComponent<Button>().onClick.AddListener(FalseAction);
+            third3.GetComponent<Button>().onClick.AddListener(TrueAction);
+            forth4.GetComponent<Button>().onClick.AddListener(FalseAction);
         }
         else if (PlayerPrefs.GetInt("CurrentLevel") == 5)
         {
@@ -57,13 +82,32 @@ public class QuestionController : MonoBehaviour
             second.SetText("3");
             third.SetText("11");
             fourth.SetText("14");
+            first1.GetComponent<Button>().onClick.AddListener(FalseAction);
+            second2.GetComponent<Button>().onClick.AddListener(FalseAction);
+            third3.GetComponent<Button>().onClick.AddListener(TrueAction);
+            forth4.GetComponent<Button>().onClick.AddListener(FalseAction);
         }
         else if (PlayerPrefs.GetInt("CurrentLevel") == 6)
         {
-            first.SetText("2");
-            second.SetText("4");
-            third.SetText("10");
-            fourth.SetText("14");
+            first.SetText("2,5");
+            second.SetText("4,1");
+            third.SetText("9,7");
+            fourth.SetText("14,1");
+            first1.GetComponent<Button>().onClick.AddListener(FalseAction);
+            second2.GetComponent<Button>().onClick.AddListener(FalseAction);
+            third3.GetComponent<Button>().onClick.AddListener(TrueAction);
+            forth4.GetComponent<Button>().onClick.AddListener(FalseAction);
         }
+    }
+    void openTruePanel()
+    {
+        Debug.Log("Tavukcu");
+        True.SetActive(true);
+        //PlayerPrefs.SetInt("CurrentLevel", PlayerPrefs.GetInt("CurrentLevel") + 1);
+    }
+    void openFalsePanel()
+    {
+        Debug.Log("Tavukcu");
+        False.SetActive(true);
     }
 }
